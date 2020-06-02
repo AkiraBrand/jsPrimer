5c0a179a9a98fd7c4fa714f05d3258a9fb0397fe
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-06-02 15:16:55 -0700

    adds more books

--- primer.js
+++ primer.js
@@ -1,13 +1,22 @@
+var bookArray = []
+
 function addFavoriteBook(bookName) {
-    bookArray = []
 if(bookName.includes("great")) {
-    /*bookArray.push(bookName); */
     console.log('ya, thats a great book');
     }
     else{bookArray.push(bookName);
-        console.log(bookArray);
+        console.log(`bookArray.length`);
     }
 };
-bookOne = "The great Gatsby"
-bookTwo = "The Bible"
-addFavoriteBook(bookTwo);
+
+var bookOne = "The great Gatsby";
+var bookTwo = "The Well-Versed programmer";
+var bookThree = "Their Eyes Were Watching God";
+var bookFour = "Kafka on the Shore";
+var bookFive = "From good to Great";
+
+// addFavoriteBook(bookOne);
+// addFavoriteBook(bookTwo);
+addFavoriteBook(bookThree);
+addFavoriteBook(bookFour);
+addFavoriteBook(bookFive);
